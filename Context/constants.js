2e6efbf7fcf7e4fd1f5a4007f4879f835be927d5
Kalash2002 <202051097@iiitvadodara.ac.in>
2023-04-16 17:29:59 +0530

    final

---
+++ Context/constants.js
@@ -0,0 +1,15 @@
+///0x5FbDB2315678afecb367f032d93F642f64180aa3
+//0x5FbDB2315678afecb367f032d93F642f64180aa3
+
+import nftMarketplace from "./NFTMarketplace.json";
+import transferFunds from "./TransferFunds.json";
+
+//NFT MARKETPLACE
+export const NFTMarketplaceAddress =
+  "0xEabFBD96dBA1FCEeC20e07fCbe2f4D96F050FC04";
+export const NFTMarketplaceABI = nftMarketplace.abi;
+
+//TRANSFER FUNDS
+export const transferFundsAddress =
+  "0xf7E7B78e853b039d7109F2f0494cE934b00e3c1E";
+export const transferFundsABI = transferFunds.abi;
